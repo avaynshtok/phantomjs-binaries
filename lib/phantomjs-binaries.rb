e58e28adc4d28943dae9e44acb3fd275e6da2b72
Anton Vaynshtok <avaynshtok@gmail.com>
2012-04-08 15:22:04 -0700

    autogen'd tree

---
+++ lib/phantomjs-binaries.rb
@@ -0,0 +1,7 @@
+require "phantomjs-binaries/version"
+
+module Phantomjs
+  module Binaries
+    # Your code goes here...
+  end
+end
---
+++ lib/phantomjs-binaries/version.rb
@@ -0,0 +1,5 @@
+module Phantomjs
+  module Binaries
+    VERSION = "0.0.1"
+  end
+end
