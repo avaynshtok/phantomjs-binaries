e264aafcd24284a76144ceb2b65d7044fd98ce46
Anton Vaynshtok <avaynshtok@gmail.com>
2015-11-08 12:12:02 -0800

    bump version

--- lib/phantomjs-binaries/version.rb
+++ lib/phantomjs-binaries/version.rb
@@ -1,6 +1,6 @@
 module Phantomjs
   module Binaries
-    PHANTOM_VERSION = "1.9.2"
-    VERSION = PHANTOM_VERSION + ".4"
+    PHANTOM_VERSION = "1.9.8"
+    VERSION = PHANTOM_VERSION + ".0"
   end
 end
